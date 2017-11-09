42472481123d07b5b5a589200bd9a44f86f4eece
Michał Karnicki <mkarnicki@gmail.com>
2017-11-09 14:18:48 +0100

    Add about page.

---
+++ resources/views/source/about.blade.php
@@ -0,0 +1,3 @@
+<div class="w-full h-full p-3 bg-blue-lightest flex flex-col justify-center items-center">
+    About
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,6 +24,7 @@
             <tailwind-fiddle
                 class="w-full h-full"
                 src-default='@include('source.default')'
+                src-about='@include('source.about')'
             >
             </tailwind-fiddle>
         </div>
