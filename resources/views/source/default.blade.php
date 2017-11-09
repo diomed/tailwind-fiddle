b18e00572daadffa5a49d4410e09a81f4ca79750
Michał Karnicki <mkarnicki@gmail.com>
2017-11-09 15:46:02 +0100

    Fix indentation.

--- resources/views/source/default.blade.php
+++ resources/views/source/default.blade.php
@@ -1,55 +1,55 @@
 <div class="w-full h-full p-3 bg-blue-lightest flex flex-col justify-center items-center">
-  <div class="flex flex-col items-center">
-    <p class="font-thin text-blue tracking-wide">Inspire and be inspired</p>
-  </div>
-
-  <div class="w-32 flex justify-between">
-
-  <div class="h-10 w-10 mt-2 bg-blue-lighter rounded-full flex justify-center items-center">
-    <div class="h-8 w-8 bg-blue rounded-full flex justify-center items-center">
-      <div class="h-6 w-6 bg-blue-dark rounded-full flex justify-center items-center">
-        <div class="h-4 w-4 bg-blue-darker rounded-full flex justify-center items-center">
-          <div class="h-2 w-2 bg-blue-darkest rounded-full flex justify-center items-center">
-
-          </div>
-        </div>
-      </div>
+    <div class="flex flex-col items-center">
+        <p class="font-thin text-blue tracking-wide">Inspire and be inspired</p>
     </div>
-  </div>
 
-  <div class="h-10 w-10 mt-2 bg-green-lighter rounded-full flex justify-center items-center">
-    <div class="h-8 w-8 bg-green rounded-full flex justify-center items-center">
-      <div class="h-6 w-6 bg-green-dark rounded-full flex justify-center items-center">
-        <div class="h-4 w-4 bg-green-darker rounded-full flex justify-center items-center">
-          <div class="h-2 w-2 bg-green-darkest rounded-full flex justify-center items-center">
+    <div class="w-32 flex justify-between">
 
-          </div>
+        <div class="h-10 w-10 mt-2 bg-blue-lighter rounded-full flex justify-center items-center">
+            <div class="h-8 w-8 bg-blue rounded-full flex justify-center items-center">
+                <div class="h-6 w-6 bg-blue-dark rounded-full flex justify-center items-center">
+                    <div class="h-4 w-4 bg-blue-darker rounded-full flex justify-center items-center">
+                        <div class="h-2 w-2 bg-blue-darkest rounded-full flex justify-center items-center">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
 
-  <div class="h-10 w-10 mt-2 bg-purple-lighter rounded-full flex justify-center items-center">
-    <div class="h-8 w-8 bg-purple rounded-full flex justify-center items-center">
-      <div class="h-6 w-6 bg-purple-dark rounded-full flex justify-center items-center">
-        <div class="h-4 w-4 bg-purple-darker rounded-full flex justify-center items-center">
-          <div class="h-2 w-2 bg-purple-darkest rounded-full flex justify-center items-center">
+        <div class="h-10 w-10 mt-2 bg-green-lighter rounded-full flex justify-center items-center">
+            <div class="h-8 w-8 bg-green rounded-full flex justify-center items-center">
+                <div class="h-6 w-6 bg-green-dark rounded-full flex justify-center items-center">
+                    <div class="h-4 w-4 bg-green-darker rounded-full flex justify-center items-center">
+                        <div class="h-2 w-2 bg-green-darkest rounded-full flex justify-center items-center">
 
-          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
+
+        <div class="h-10 w-10 mt-2 bg-purple-lighter rounded-full flex justify-center items-center">
+            <div class="h-8 w-8 bg-purple rounded-full flex justify-center items-center">
+                <div class="h-6 w-6 bg-purple-dark rounded-full flex justify-center items-center">
+                    <div class="h-4 w-4 bg-purple-darker rounded-full flex justify-center items-center">
+                        <div class="h-2 w-2 bg-purple-darkest rounded-full flex justify-center items-center">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
-  </div>
 
-  </div>
-
-  <div class="mt-2 flex flex-col items-center">
-    <p class="font-thin text-blue">
-        <a class="no-underline text-xs text-blue hover:text-blue-dark uppercase font-sans font-semibold hover:font-bold"
+    <div class="mt-2 flex flex-col items-center">
+        <p class="font-thin text-blue">
+            <a class="no-underline text-xs text-blue hover:text-blue-dark uppercase font-sans font-semibold hover:font-bold"
             target="_blank"
             rel="nofollow noopener"
             href="https://tailwindcss.com/">Tailwind CSS
-        </a>
-    </p>
-  </div>
+            </a>
+        </p>
+    </div>
 </div>
