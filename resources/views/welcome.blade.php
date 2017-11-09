8586820b5d0a1d33950ab6ae36525dada9e39bdb
Michał Karnicki <mkarnicki@gmail.com>
2017-11-09 02:08:45 +0100

    Basic layout.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,20 +7,25 @@
 
         <title>TailwindCSS fiddle</title>
 
-
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/preflight.min.css" rel="stylesheet">
-
         <style>
             html, body {
                 height: 100vh;
                 margin: 0;
             }
         </style>
-
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/utilities.min.css" rel="stylesheet">
     </head>
 
-    <body>
+    <body id="app" class="flex flex-col">
+        <div class="fiddle w-full h-auto overflow-hidden overflow-y-scroll">
+        </div>
 
+        <div class="w-full flex flex-col items-center" style="height: 40rem;">
+            <textarea class="w-full flex-1" name="name" rows="8" cols="80"></textarea>
+            <div class="h-8 flex items-center">
+                <span class="text-xs">Made with 🎵 by <a class="antialiased no-underline text-blue" href="https://twitter.com/mkarnicki"><strong>@mkarnicki</strong></a></span>
+            </div>
+        </div>
     </body>
 </html>
