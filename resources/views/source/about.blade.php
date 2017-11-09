e2018611494d04bc52a57807efdf2ef07c5dd823
Michał Karnicki <mkarnicki@gmail.com>
2017-11-09 15:45:38 +0100

    Update about page.

--- resources/views/source/about.blade.php
+++ resources/views/source/about.blade.php
@@ -1,3 +1,30 @@
-<div class="w-full h-full p-3 bg-blue-lightest flex flex-col justify-center items-center">
-    About
+<div class="w-full p-3 flex justify-center items-center">
+    <div class="w-full mx-auto md:4/5 lg:w-3/5 xl:1/2 p-8 border border-blue-lighter border-1 text-grey-darker leading-normal">
+        <p>
+            Hey! Thanks for checking out <span class="font-semibold text-blue">Tailwind fiddle</span> by
+            <a class="text-blue no-underline hover:text-purple" href="https://twitter.com/mkarnicki">Michał Karnicki</a>.
+        </p>
+        <p class="mt-3">
+            This site allows you to fiddle and play with HTML and CSS Tailwind-style with instant visual feedback. It&rsquo;s like your editor + browser in one, but you don&rsquo;t have to save your source each time you want to actually see it. Sweet! 😎
+        </p>
+        <p class="mt-3">
+            I really had fun building this site. Actually, large part of it has been created with Tailwind&nbsp;Fiddle itself!
+        </p>
+        <p class="mt-8">
+            Tailwind is a utility-first CSS framework for rapidly building custom user interfaces by:
+            <ul class="mx-2 my-4 list-reset flex flex-row justify-around">
+                <li><a class="text-blue no-underline hover:text-purple" href="https://twitter.com/adamwathan">Adam Wathan</a></li>
+                <li><a class="text-blue no-underline hover:text-purple" href="https://twitter.com/reinink">Jonathan Reinink</a></li>
+                <li><a class="text-blue no-underline hover:text-purple" href="https://twitter.com/davidhemphill">David Hemphill</a></li>
+                <li><a class="text-blue no-underline hover:text-purple" href="https://twitter.com/steveschoger">Steve Schoger</a></li>
+            </ul>
+            Go follow these awesome guys. You can also check out
+            <a class="text-blue no-underline hover:text-purple" href="https://twitter.com/tailwindcss">TailwindCSS</a> on Twitter.
+        </p>
+        <div class="flex justify-left xl:justify-center">
+            <p class="w-full xl:w-3/5 mt-8 xl:text-center">
+                I intend to put the source up on GitHub shortly, but you can already view all of it in your browser 🎉. Enjoy!
+            </p>
+        </div>
+    </div>
 </div>
