9d9584ee4e0f47e5d3e378395348a2178901caa7
Michał Karnicki <mkarnicki@gmail.com>
2017-11-13 22:36:07 +0100

    Update about page and the placeholder text.

--- resources/views/source/placeholder.blade.php
+++ resources/views/source/placeholder.blade.php
@@ -1,3 +1,3 @@
-Your awesome HTML with Tailwind CSS goes here. Hit Demo or About above to see what&rsquo;s this about 💡
+Your awesome HTML with Tailwind CSS goes here. Hit menu items above to see what&rsquo;s this about 💡
 
 You&rsquo;ll only see this message once, so you don&rsquo;t have to hit CLEAR every time!
