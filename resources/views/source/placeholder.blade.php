b9b4016142e21f91e0a20c15d8cc40a524520de4
Michał Karnicki <mkarnicki@gmail.com>
2017-11-14 01:11:23 +0100

    Update placeholder text to include shortcuts.

--- resources/views/source/placeholder.blade.php
+++ resources/views/source/placeholder.blade.php
@@ -1,3 +1,5 @@
-Your awesome HTML with Tailwind CSS goes here. Hit menu items above to see what&rsquo;s this about 💡
-
-You&rsquo;ll only see this message once, so you don&rsquo;t have to hit CLEAR every time!
+<div class="h-full flex flex-col justify-center items-center">
+    <p>Your awesome HTML with Tailwind CSS goes here. Hit menu items to see what’s this about 💡</p>
+    <p>You’ll only see this message once, so you don’t have to hit CLEAR every time!</p>
+    <p>Shortcuts: <em>tab</em> for emmet, <em>ctrl+space</em> for code autocompletion</p>
+</div>
