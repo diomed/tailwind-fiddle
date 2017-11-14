b799047ca18f26f316a3b6e0648afa94e9aee091
Michał Karnicki <mkarnicki@gmail.com>
2017-11-14 21:51:26 +0100

    Add gist loading when referrer is a gist.

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    request()->headers->set('Referer', 'https://gist.github.com/adamwathan/513d476de6c5d6213c0cd9c62f4ac154');
+
+    $referrer = request()->headers->get('Referer');
+    $gist = '';
+
+    if (isset($referrer) && starts_with($referrer, 'https://gist.github.com/')) {
+        // TODO: replace with proper regex
+        $gist = str_after($referrer, 'https://gist.github.com/');
+        $gist = str_after($gist, '/');
+        $gist = str_before($gist, '/');
+    }
+
+    return view('welcome', ['gistId' => $gist]);
 });
